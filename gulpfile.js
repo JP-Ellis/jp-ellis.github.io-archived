3e3ec107145cac59d59e0c5c3c6cea2d523fb70e
JP-Ellis <josh@jpellis.me>
2018-03-30 22:23:19 +1100

    Update submodules and compass -> sass
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp         = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
-var compass      = require('gulp-compass');
+var sass         = require('gulp-sass');
 var del          = require('del');
 var minifyCss    = require('gulp-cssnano');
 var minifyJs     = require('gulp-minify');
@@ -10,22 +10,17 @@ var livereload   = require('gulp-livereload');
 var wait         = require('gulp-wait');
 
 var paths = {
-    compass: ['css/*.scss'],
+    sass: ['css/**/*.scss'],
     fonts: ['fonts/*.woff2', 'fonts/*.woff', 'fonts/*.ttf'],
     gulp: ['gulpfile.js'],
     images: ['images/**/*.png', 'images/**/*.jpg', 'images/**/*.jpeg', 'images/**/*.svg']
 };
 
-gulp.task('css', function() {
-    gulp.src(paths.compass)
-        .pipe(compass({sass: 'css',
-                       css: 'static/css',
-                       image: 'static/img',
-                       js: 'static/js',
-                       font: 'static/font',
-                       import_path: ['./submodules'],
-                       bundle_exec: true,
-                       comments: true}))
+gulp.task('css', function(done) {
+    gulp.src(paths.sass)
+        .pipe(sass({
+            includePaths: ['./submodules']
+        }))
         .pipe(autoprefixer({browsers: ['> 5%']}))
         .pipe(minifyCss())
         .pipe(vinylPaths(del))
@@ -33,34 +28,38 @@ gulp.task('css', function() {
         .pipe(gulp.dest('static/css'))
         .pipe(wait(1000))
         .pipe(livereload());
+    done();
 });
 
-gulp.task('fonts', function() {
+gulp.task('fonts', function(done) {
     gulp.src(paths.fonts)
         .pipe(gulp.dest('static/fonts'));
+    done();
 });
 
-gulp.task('images', function() {
+gulp.task('images', function(done) {
     gulp.src(paths.images)
         .pipe(gulp.dest('static/images'))
         .pipe(livereload());
+    done();
 });
 
-gulp.task('submodules', ['share-button']);
-
-gulp.task('share-button', function() {
+gulp.task('share-button', function(done) {
     gulp.src('submodules/share-button/dist/*.min.css')
         .pipe(gulp.dest('static/css'));
     gulp.src('submodules/share-button/dist/*.min.js')
         .pipe(gulp.dest('static/js'));
+    done();
 });
 
-gulp.task('dist', ['css', 'fonts', 'images', 'submodules']);
+gulp.task('submodules', gulp.parallel('share-button'));
+
+gulp.task('dist', gulp.parallel('css', 'fonts', 'images', 'submodules'));
 
 gulp.task('watch', function() {
     livereload.listen();
-    gulp.watch(paths.compass, ['css']);
-    gulp.watch(paths.images, ['images']);
+    gulp.watch(paths.sass, gulp.series('css'));
+    gulp.watch(paths.images, gulp.series('images'));
 });
 
-gulp.task('default', ['dist', 'watch']);
+gulp.task('default', gulp.series('dist', 'watch'));
